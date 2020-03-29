6ef56325903e362768c2bff3cb570aabf037526f
Yuta-Mitani-sys <my1140540@gmail.com>
2020-03-29 17:01:14 +0900

    new-action-create

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,21 @@
 class MemosController < ApplicationController
+  
   def index
     @memos = Memo.all
   end
+
+  def new
+    @memo = Memo.new
+  end
+
+  def create
+    binding.pry
+    Memo.create(memo_params)
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:title, :text).merge(user_id: current_user.id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "memos#index"
-  resources :memos, only: [:index]
+  resources :memos, only: [:index, :new, :create]
   resources :users, only: [:edit, :update, :show]
 end
